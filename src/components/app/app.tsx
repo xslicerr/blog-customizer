416f87563e02f85e2f27b99a0f376302e24dd395
Nikita Korchagin <slicerghost1@yandex.ru>
2026-09-14 19:00:25 +0400

    feat: complete React app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,49 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState, useRef } from 'react';
 import clsx from 'clsx';
+import { ArrowButton } from 'src/ui/arrow-button';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './../../constants/articleProps';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [appliedState, setAppliedState] =
+		useState<ArticleStateType>(defaultArticleState);
+	const [isOpenForm, setIsOpenForm] = useState<boolean>(false);
+	const mainRef = useRef<HTMLDivElement>(null);
+	useOutsideClickClose({
+		isOpen: isOpenForm,
+		rootRef: mainRef,
+		onChange: setIsOpenForm,
+	});
+
+	const handleOpenFormClick = (): void => {
+		setIsOpenForm((prev) => !prev);
+	};
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appliedState.fontFamilyOption.value,
+					'--font-size': appliedState.fontSizeOption.value,
+					'--font-color': appliedState.fontColor.value,
+					'--container-width': appliedState.contentWidth.value,
+					'--bg-color': appliedState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArrowButton isOpen={isOpenForm} onClick={handleOpenFormClick} />
+			<ArticleParamsForm
+				onApply={setAppliedState}
+				isOpenForm={isOpenForm}
+				ref={mainRef}
+			/>
 			<Article />
 		</main>
 	);
